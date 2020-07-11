82345cc609a8f97ce3b14296dde1ce871986e5b7
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2020-07-11 13:55:02 +0400

    button enabling/disabling implemented

--- Views/TitusView.xaml.cs
+++ Views/TitusView.xaml.cs
@@ -7,14 +7,27 @@ namespace Context_Processor.Views
 {
     public class TitusView : UserControl
     {
+        private Button unitInsertButton;
+        private Button semanticInsertButton;
+
         public TitusView()
         {
             InitializeComponent();
         }
 
+        public void UnitInsert(object sender, RoutedEventArgs e)
+        {            
+            // do insertion
+            unitInsertButton.IsEnabled = false;
+            semanticInsertButton.IsEnabled = true;
+        }
+
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+            unitInsertButton = this.FindControl<Button>("UnitBtn");
+            semanticInsertButton = this.FindControl<Button>("SemBtn");
+            semanticInsertButton.IsEnabled = false;
         }
         
     }
