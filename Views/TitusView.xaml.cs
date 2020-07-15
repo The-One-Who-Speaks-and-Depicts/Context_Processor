86a987692beefaf2ea1e8e68519d5909d93f700e
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2020-07-15 16:48:36 +0400

    readonly/not-ro textbox regulations on click for unit field

--- Views/TitusView.xaml.cs
+++ Views/TitusView.xaml.cs
@@ -9,6 +9,8 @@ namespace Context_Processor.Views
     {
         private Button unitInsertButton;
         private Button semanticInsertButton;
+        private TextBox unitField;
+        private TextBox semanticsField;
         private TextBox finalField;
 
         public TitusView()
@@ -22,6 +24,8 @@ namespace Context_Processor.Views
             transferrable += "<i>" + richTextBox1.Text + "</i>";
             transferrable += ".<br>\n";
             richTextBox5.Text += transferrable;*/
+            unitField.IsReadOnly = true;
+            semanticsField.IsReadOnly = false;
             unitInsertButton.IsEnabled = false;
             semanticInsertButton.IsEnabled = true;
         }
@@ -31,6 +35,8 @@ namespace Context_Processor.Views
             AvaloniaXamlLoader.Load(this);
             unitInsertButton = this.FindControl<Button>("UnitBtn");
             semanticInsertButton = this.FindControl<Button>("SemBtn");
+            unitField = this.FindControl<TextBox>("UnitTextBox");
+            semanticsField = this.FindControl<TextBox>("SemanticsTextBox");
             finalField = this.FindControl<TextBox>("FinalTextBox");
         }
         
