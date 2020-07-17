55d64cec39030a1f6bba56622bbb671be25183b3
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2020-07-17 13:27:20 +0400

    button and textbox for basement of analysis insertion are connected with back-end

--- Views/TitusView.xaml.cs
+++ Views/TitusView.xaml.cs
@@ -17,6 +17,7 @@ namespace Context_Processor.Views
         private Button semanticInsertButton;
         private Button contextsAmountInsertionButton;
         private Button contextInsertionButton;
+        private Button analysisBasementInsertionButton;
 
         //create variables for operating with text boxes
         private TextBox unitField;
@@ -24,6 +25,7 @@ namespace Context_Processor.Views
         private NumericUpDown contextsAmountField;
         private TextBox sourceField;
         private TextBox contextField;
+        private TextBox analysisBasementField;
         private TextBox finalField;
 
         public TitusView()
@@ -116,12 +118,14 @@ namespace Context_Processor.Views
             semanticInsertButton = this.FindControl<Button>("SemBtn");
             contextsAmountInsertionButton = this.FindControl<Button>("NumBtn");
             contextInsertionButton = this.FindControl<Button>("ContextBtn");
+            analysisBasementInsertionButton = this.FindControl<Button>("BasementBtn");
             //initialize text boxes
             unitField = this.FindControl<TextBox>("UnitTextBox");
             semanticsField = this.FindControl<TextBox>("SemanticsTextBox");
             contextsAmountField = this.FindControl<NumericUpDown>("NumeralBox");
             sourceField = this.FindControl<TextBox>("SourceTextBox");
             contextField = this.FindControl<TextBox>("ContextTextBox");
+            analysisBasementField = this.FindControl<TextBox>("BasementTextBox");
             finalField = this.FindControl<TextBox>("FinalTextBox");
         }
         
