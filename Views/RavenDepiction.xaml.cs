9b789f5c625dec7ca225a7b4adfc1ea267f984ae
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-03-06 14:49:56 +0400

    ComboBox interface for database view implemented

--- Views/RavenDepiction.xaml.cs
+++ Views/RavenDepiction.xaml.cs
@@ -1,16 +1,28 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Linq;
+using ReactiveUI;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.Interactivity;
+using Avalonia.ReactiveUI;
 using Raven.Client;
 using Raven.Client.Documents;
+using Context_Processor.Models;
+using MessageBox.Avalonia;
+using MessageBox.Avalonia.Enums;
+using MessageBox.Avalonia.DTO;
+
 
 
 namespace Context_Processor.Views
 {
     public class RavenDepiction : Window
-    {        
+    {
+        private ComboBox unitsComboBox;
 
         public RavenDepiction()
         {            
@@ -19,7 +31,24 @@ namespace Context_Processor.Views
 
         private void InitializeComponent()
         {
-            AvaloniaXamlLoader.Load(this);
+            AvaloniaXamlLoader.Load(this);            
+            unitsComboBox = this.FindControl<ComboBox>("UnitsComboBox");
+            unitsComboBox.Items = RavenGet().Select(unit => unit.name);            
+        }
+
+        public List<Unit> RavenGet()
+        {
+            var store = new DocumentStore 
+            {
+                Urls = new string[]{"http://localhost:8080"},
+                Database = "UnitsDB"
+            };
+            store.Initialize();
+            using (var session = store.OpenSession())
+            {
+                List<Unit> units = session.Advanced.RawQuery<Unit>("from Units").ToList();
+                return units;      
+            }            
         }
     }
 }
