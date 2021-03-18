158ac91b662e0d20b4f8ecc8138ab4ed93580af0
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-03-18 21:43:07 +0400

    ADD: delete and localization buttons for RavenDB window

--- Views/RavenDepiction.xaml.cs
+++ Views/RavenDepiction.xaml.cs
@@ -22,7 +22,16 @@ namespace Context_Processor.Views
 {
     public class RavenDepiction : Window
     {
+        // create variable for selector of units
         private ComboBox unitsComboBox;
+
+        //create variables for buttons
+        private Button deleteButton;
+        private Button localizationButton;
+
+        //creates strings for localization
+        private string deletionLocalized = "Удалить";
+        private string localizationLocalized = "ru";
 
         public RavenDepiction()
         {            
@@ -31,11 +40,18 @@ namespace Context_Processor.Views
 
         private void InitializeComponent()
         {
-            AvaloniaXamlLoader.Load(this);            
+            AvaloniaXamlLoader.Load(this); 
+            // get names for each unit           
             unitsComboBox = this.FindControl<ComboBox>("UnitsComboBox");
-            unitsComboBox.Items = RavenGet().Select(unit => unit.name);            
+            unitsComboBox.Items = RavenGet().Select(unit => unit.name);
+            // initialize buttons
+            deleteButton = this.FindControl<Button>("DeleteBtn");
+            deleteButton.Content = deletionLocalized;
+            localizationButton = this.FindControl<Button>("LocalizationBtn");
+            localizationButton.Content = localizationLocalized;            
         }
 
+        // gets units from RavenDB
         public List<Unit> RavenGet()
         {
             var store = new DocumentStore 
@@ -49,6 +65,16 @@ namespace Context_Processor.Views
                 List<Unit> units = session.Advanced.RawQuery<Unit>("from Units").ToList();
                 return units;      
             }            
+        }
+
+        public void DeleteUnit(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void Localize (object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
