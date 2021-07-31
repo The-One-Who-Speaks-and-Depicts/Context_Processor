d38aaca08558f5bbf493bffbd561819f95dc7591
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-08-01 00:04:17 +0400

    DELETE: debugging artifacts

--- Views/TitusView.xaml.cs
+++ Views/TitusView.xaml.cs
@@ -241,7 +241,6 @@ namespace Context_Processor.Views
         {
             XDocument doc = XDocument.Load(filePath);
             XElement el = XElement.Parse(XMLToHTML(finalField.Text));
-            Console.WriteLine(doc.Descendants());
             XElement parentElement = doc.Descendants("div").Where(x => x.Attribute("class").Value == "analyzedUnit").LastOrDefault();
             if (parentElement != null) parentElement.AddAfterSelf(el);
             doc.Save(filePath);
